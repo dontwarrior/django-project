848145327dab3e650f5d4e0074ee4db244d07602
dontwarrior <57672484+dontwarrior@users.noreply.github.com>
2020-11-28 16:38:44 +0200

    new_project

---
+++ django_prj/blog/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+from blog.models import Post
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'content', 'date_posted']
+    list_filter = ['date_posted']
+
