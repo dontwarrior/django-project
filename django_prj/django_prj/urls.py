c0f2b23d52927a52bf8138525029507b4b814c0b
dontwarrior <57672484+dontwarrior@users.noreply.github.com>
2020-11-28 17:19:14 +0200

    user_urls_added

--- django_prj/django_prj/urls.py
+++ django_prj/django_prj/urls.py
@@ -16,28 +16,18 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.contrib.auth import views as auth_views
 from django.urls import path, include
 
-from users import views as user_view
 
 urlpatterns = [
                   path('admin/', admin.site.urls),
                   path('', include('blog.urls')),
-                  path('register/', user_view.register, name='register'),
-                  path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-                  path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
-                  path('password-reset/',
-                       auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'),
-                       name='password_reset'),
-                  path('password-reset/done/',
-                       auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'),
-                       name='password_reset_done'),
-                  path('password-reset-confirm/<uidb64>/<token>/',
-                       auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'),
-                       name='password_reset_confirm'),
-                  path('password-reset-complete/',
-                       auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'),
-                       name='password_reset_complete'),
-                  path('profile/', user_view.profile, name='profile'),
+                  path('register/', include('users.urls')),
+                  path('login/', include('users.urls')),
+                  path('logout/', include('users.urls')),
+                  path('password-reset/', include('users.urls')),
+                  path('password-reset/done/', include('users.urls')),
+                  path('password-reset-confirm/<uidb64>/<token>/', include('users.urls')),
+                  path('password-reset-complete/', include('users.urls')),
+                  path('profile/',include('users.urls')),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
