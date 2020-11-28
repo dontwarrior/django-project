c9a4d2b3f427a23f732f89307fb4f931b165f8dd
dontwarrior <57672484+dontwarrior@users.noreply.github.com>
2020-11-28 17:38:05 +0200

    automatic login_after_registration

--- django_prj/users/views.py
+++ django_prj/users/views.py
@@ -1,4 +1,5 @@
 from django.contrib import messages
+from django.contrib.auth import login, authenticate
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 
@@ -12,8 +13,11 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Your account has been created {username}!. Now you are able to login.')
-            return redirect('login')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, f'Your account has been created {username}!')
+            return redirect('blog-home')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
